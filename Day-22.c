4b375eecf40d226901e18ad65de2248762a7f545
pragya159 <pragya.singh2025@glbajajgroup.org>
2026-07-09 10:25:02 +0530

    Day-22 uploaded

---
+++ Day-22.c
@@ -0,0 +1,32 @@
+﻿// Q1 "Check Palindrome String"
+
+#include <stdio.h>
+
+int main() {
+    char str;
+    int len = 0, i, is_palindrome = 1; // 1 means true
+    
+    printf("Enter a string: ");
+    scanf("%s", str);
+    
+    // Calculate length
+    while (str[len] != '\0') {
+        len++;
+    }
+    
+    // Compare characters from both ends moving inward
+    for (i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            is_palindrome = 0; // false
+            break;
+        }
+    }
+    
+    if (is_palindrome == 1) {
+        printf("The string is a palindrome.\n");
+    } else {
+        printf("The string is NOT a palindrome.\n");
+    }
+    
+    return 0;
+}
